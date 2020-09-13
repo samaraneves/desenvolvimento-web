eda04b3a5744f225c6ac6179027ea35ab8cd926e
Samara <sneves0@hotmail.com>
2020-09-13 19:28:51 -0300

    Bhaskara

--- exercicios/exercicios-funcao/exercicio04.js
+++ exercicios/exercicios-funcao/exercicio04.js
@@ -1,19 +1,19 @@
 // 04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
 // e o resto da divisão destes dois valores.
-let divisor = (dividendo, divisor) => {
-    return (
-        `
-            divisão: ${dividendo / (divisor <= 0 ? 'Não é possível dividir por 0' : divisor)}
-            resto: ${dividendo % divisor}
-        `
-    )
+function divindo(dividendo, divisor) {
+    if (divisor === 0) {
+        return console.log('Não é possível realizar divisão por 0.')
+    } else {
+        return console.log(
+            `Resultado: ${dividendo/divisor}
+         Resto: ${dividendo%divisor}`)
+    }
 }
-console.log(divisor(2, 0))
-
-//Resolução Leitão
+divindo(2, 2)
+    //Resolução Leitão
 function divisao(dividendo, divisor) {
     console.log("Resultado: " + Math.floor(dividendo / divisor));
     console.log(`Resto: ${dividendo % divisor}`)
 }
 
-divisao(11, 4)
+divisao(2, 0)
